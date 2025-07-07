54010a6e40ab4533684a5fde6cce458adb69f94f
Aryan Gandhi <me23btech11010@iith.ac.in>
2025-07-07 10:42:51 +0100

    Update cron.js

--- scripts/cron.js
+++ scripts/cron.js
@@ -98,8 +98,8 @@ async function main() {
   const PROMPT_OVERHEAD_TOKENS = 300;
 
   const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
   );
 
   const transporter = nodemailer.createTransport({
